d07c630c919428ae1667abfb9141b9aa322b7366
aievey <amitabh283verma@gmail.com>
2026-01-15 11:21:08 +0530

    cover update

--- src/components/cover.jsx
+++ src/components/cover.jsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 export default function Cover() {
   return (
-    <div className="relative w-full h-[94vh] bg-black md:flex justify-center items-center">
-      <div className="h-3/10 md:h-9/10 md:w-4/10 flex justify-center items-center">
+    <div className="relative w-full center max-w-480 h-[94vh] max-h-270 bg-black md:flex justify-center items-center">
+      {/* leftcolumn */}
+      <div className="h-3/10 md:h-full md:w-4/10 flex justify-center items-center md:justify-end">
         <div className="">
           <h2 className="text-center md:text-right text-white font-bold py-5 text-4xl md:text-4xl/1">
             Business Brewery
@@ -12,20 +13,23 @@ export default function Cover() {
           </h1>
         </div>
       </div>
-      <div className=" h-7/10 md:h-9/10 md:w-6/10 ">
-        <div className="relative aspect-6/4 overflow-hidden">
+      {/* right Column */}
+      <div className="relative h-7/10 md:h-full md:w-6/10 ">
+        <div className="relative h-full overflow-hidden">
           <Image
-            src="/cover-main.avif"
-            height={500}
-            width={500}
+            src="/business-brewery-cover-main.avif"
+            height={1200}
+            width={1200}
             priority
-            alt="business-brewery-cover aspect-auto"
-            className="aspect-6/4 h-full w-full object-cover"
+            alt="business-brewery-cover "
+            className="aspect-auto h-full object-cover z-0"
           />
         </div>
-        <p className="text-center text-l shadow bg-amber-100 p-4 md:p-6 border-0 -mb-4 rounded-2xl">
-          Turn your business into a high-speed revenue engine.
-        </p>
+        <div className="w-full absolute -bottom-5 left-1/2 transform -translate-x-1/2 flex justify-center items-center text-l shadow bg-white/90 h-1/11 py-10 xl:p-0 rounded-2xl z-10 md:w-1/2 backdrop-blur-[5px]">
+          <p className="text-center px-10">
+            Turn your business into a high-speed revenue engine.
+          </p>
+        </div>
       </div>
     </div>
   );
